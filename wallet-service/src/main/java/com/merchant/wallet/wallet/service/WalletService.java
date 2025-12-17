cbf7cf2441105c326bb135dce6ec9f3093a7ae1a
Odwa <4050682+odwaz@users.noreply.github.com>
2025-12-17 20:13:37 +0200

    Remove unused imports from WalletService

--- wallet-service/src/main/java/com/merchant/wallet/wallet/service/WalletService.java
+++ wallet-service/src/main/java/com/merchant/wallet/wallet/service/WalletService.java
@@ -2,14 +2,12 @@ package com.merchant.wallet.wallet.service;
 
 import com.merchant.wallet.wallet.domain.Wallet;
 import com.merchant.wallet.wallet.repository.WalletRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
